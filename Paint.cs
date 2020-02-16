c1e4a5cdbb5a2bc44815922616d51c434e6e1333
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2020-02-16 20:46:55 +0300

    Добавьте файлы проекта.

---
+++ Paint.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algoritms
+{
+    class Paint
+    {
+        int[,] map;
+        int w, h;
+
+        public Paint(int w, int h)
+        {
+            this.w = w;
+            this.h = h;
+            map = new int[w, h];
+        }
+
+        void SetMap(int x, int y, int number)
+        {
+            if (x < 0 || x >= w) return;
+            if (y < 0 || x >= h) return;
+            map[x, y] = number;
+            PrintAt(x, y);
+        }
+
+        void PrintAt(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(map[x, y]);
+        }
+
+        public void PutRandomNumber(int count)
+        {
+            Random random = new Random();
+            for (int j = 0; j < count; j++)
+                SetMap(random.Next(w), random.Next(h), 1);
+        }
+
+        //42:35
+        
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace algoritms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Paint paint = new Paint(40,20);
+            paint.PutRandomNumber(100);
+            Console.ReadKey();
+        }
+    }
+}
